f5ab72669621f005ea7222cfb7a1ad20430527f7
caigaoxing <caigaoxing@douyu.tv>
2021-01-28 22:01:20 +0800

    add 300

---
+++ leetcode/300.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return 1
+	}
+	var dp []int
+	for i := 0; i < len(nums); i++ {
+		dp = append(dp, 1)
+	}
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			dp[i] = dp[i-1] + 1
+		}
+
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				if dp[i] < dp[j]+1 {
+					dp[i] = dp[j] + 1
+				}
+			}
+		}
+	}
+
+	var max = 1
+	for i := 0; i < len(dp); i++ {
+		if dp[i] > max {
+			max = dp[i]
+		}
+	}
+	return max
+}
